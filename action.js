e6440f83e041b7b7815ca5bad05b4b7fa8f1f5ae
Rajeev Ranjan Jha <rajeev.jha2014@teachforindia.org>
2016-09-03 18:11:50 +0530

    Phonic chunks created on pressing the button. Alert message instead of sound file.

---
+++ action.js
@@ -0,0 +1,32 @@
+function chunk() {
+  //empty the phonemeDiv of previous buttons
+  var phonemeDiv = document.getElementsByClassName('phonemeDiv')[0];
+  phonemeDiv.innerHTML ="";
+
+  //read the word from the input box
+  var word= document.getElementById('word').value;
+  //parse the sampledb.json file
+  $.getJSON("sampledb.json", function(db){
+    //for each phoneme in the word
+    for (var item in db[word].chunks) {
+      //create a button. pass each item in chunks and phones as argument
+      createButton(db[word].chunks[item],db[word].phones[item]);
+    }
+  });
+}
+
+function createButton(phoneme, sound) {
+  //create a button
+  var button = document.createElement("button");
+  button.innerHTML = phoneme;
+
+  //append the button to the phonemeDiv
+  var phonemeDiv = document.getElementsByClassName('phonemeDiv')[0];
+  phonemeDiv.appendChild(button);
+
+  //add event handler for each button
+  button.addEventListener("click",function(){
+    //play sound
+    alert (sound);
+  });
+}
