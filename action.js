cdbe35ab590230e9470a344d02bf8f49fecd2013
vinaypuppal <me@vinaypuppal.com>
2016-09-07 01:14:10 +0530

    used JQuery to handel dom instead of native JS

--- action.js
+++ action.js
@@ -1,22 +1,36 @@
-$(document).keyup(function (e) {
-    if ($("#word").is(":focus") && (e.keyCode == 13)) {
-        chunk();
+$(document).ready(function(){
+
+  $('.form').on('submit', function(e){
+    // to prevent default form submit action
+    e.preventDefault();
+    // get input value and trim white spaces to left and right if any
+    var inputValue = $('#word').val().trim();
+    // check if inputValue is not empty
+    if(inputValue){
+      chunk(inputValue);
+    }else{
+      alert("Enter A Word");
     }
+  });
+
 });
 
-function chunk() {
-  //empty the phonemeDiv of previous buttons
-  var phonemeDiv = document.getElementsByClassName('phonemeDiv')[0];
-  phonemeDiv.innerHTML ="";
-
-  //read the word from the input box
-  var word= document.getElementById('word').value;
+function chunk(word) {
+  //empty the buttons of letters of previous word
+  $('.js-phonemes').empty();
   //convert the word into lowercase
-  word = word.toLowerCase().trim();
-  $("#theword").text(word);
+  word = word.toLowerCase();
+  $(".js-word").text(word);
+
+  //show loading text
+  $('.js-phonemes').text('Loading Please Wait!...');
+
   //parse the sampledb.json file
   $.getJSON("sampledb.json", function(db){
+    // remove loading text
+    $('.js-phonemes').text('');
     //for each phoneme in the word
+    //check if word is in our db if not throw error
     if (typeof db[word] != "undefined") {
       for (var item in db[word].chunks) {
         //create a button. pass each item in chunks and phones as argument
@@ -30,17 +44,8 @@ function chunk() {
 }
 
 function createButton(phoneme, sound) {
-  //create a button
-  var button = document.createElement("button");
-  button.innerHTML = phoneme;
-  button.className = "phoneme";
-  //append the button to the phonemeDiv
-  var phonemeDiv = document.getElementsByClassName('phonemeDiv')[0];
-  phonemeDiv.appendChild(button);
-
-  //add event handler for each button
-  button.addEventListener("click",function(){
-    //play sound
-    alert (sound);
-  });
+  //create a button with text, className: 'phoneme' and click handler
+  var button = $('<button>').text(phoneme).addClass('phoneme').on('click',function(){alert(sound)});
+  //append the button to the phonemes
+  $('.js-phonemes').append(button);
 }
